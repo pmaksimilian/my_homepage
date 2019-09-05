2451d5df8568ad7e5d66190a2a6b175de58e49c9
Maksimilian Pavsic <pavsicmaks@gmail.com>
2019-09-05 18:35:33 +0200

    Added effects to picture and KDO SEM

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
+$(".naslovna-efekt").fadeIn(4000);
+
+$(".naslovna-tekst").fadeIn(2500);
+
+$(".naslovna-tekst").animate({
+  left: '+=70%',
+}, 3000, function() {
+    // Animation complete.
+});
+
 $(".pokaziKdo").click(function() {
     $(".skritiKdo").slideDown("slow");
+    $(".desni").show("slide", { direction: "right" }, 1500);
+    $(".levi").show("slide", { direction: "left" }, 1500);
     $(".skrijKdo").show();
     $(this).hide();
 });
 
 $(".skrijKdo").click(function() {
     $(".skritiKdo").slideUp("slow");
+    $(".desni").hide();
+    $(".levi").hide();
     $(".pokaziKdo").show();
     $(this).hide();
 });
